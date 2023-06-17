19048acb8f3617a495ab792c85f1afde64bb84e1
Korotaev Maxim <129674364+GN0Mchik@users.noreply.github.com>
2023-06-17 21:06:30 +0700

    Update burger.js

--- burger.js
+++ burger.js
@@ -1,24 +1,35 @@
-const BURGER_OPENED_CLASSNAME = 'burger_open';
-const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
-const BODY_FIXED_CLASSNAME = 'body_fixed';
+const BURGER_OPENED_CLASSNAME = "burger_open";
+const BURGER_BTN_OPENED_CLASSNAME = "active";
+const BODY_FIXED_CLASSNAME = "body_fixed";
+const BURGER_NAV_CLASSNAME = "burger_open";
 
-const bodyNode = document.querySelector('body');
-const burgerNode = document.querySelector('.js-burger');
-const burgerBtnNode = document.querySelector('.js-burger-btn');
-const burgerContentNode = document.querySelector('.js-burger__content')
+const bodyNode = document.querySelector("body");
+const burgerNode = document.querySelector(".js-burger");
+const burgerBtnNode = document.querySelector(".js-burger-btn");
+const burgerContentNode = document.querySelector(".js-burger__content");
+const burgerLinkNode = document.querySelector(".js-link");
 
-burgerBtnNode.addEventListener('click', toggleBurger);
+burgerBtnNode.addEventListener("click", toggleBurger);
 
-burgerNode.addEventListener('click', (event) => {
-    const isClickOutsideContent = !event.composedPath().includes(burgerContentNode)
+burgerNode.addEventListener("click", (event) => {
+  const isClickOutsideContent = !event
+    .composedPath()
+    .includes(burgerContentNode);
 
-    if (isClickOutsideContent) {
-        toggleBurger();
-    }
-})
+  if (isClickOutsideContent) {
+    toggleBurger();
+  }
+});
 
 function toggleBurger() {
-    burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
-    bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
-    burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+  burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
+  bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+  burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+  burgerLinkNode.classList.toggle(BURGER_NAV_CLASSNAME);
 }
+
+document.querySelectorAll("[burger_link]").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburgerBtn.classList.remove("js-link");
+  })
+);
