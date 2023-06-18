7804892cd39b17ad448a9fa3a3d6a295ca268956
Korotaev Maxim <129674364+GN0Mchik@users.noreply.github.com>
2023-06-18 13:25:52 +0700

    Update burger.js

--- burger.js
+++ burger.js
@@ -1,13 +1,11 @@
 const BURGER_OPENED_CLASSNAME = "burger_open";
 const BURGER_BTN_OPENED_CLASSNAME = "active";
-const BODY_FIXED_CLASSNAME = "body_fixed";
-const BURGER_NAV_CLASSNAME = "burger_open";
+const BODY_FIXED_CLASSNAME_2 = "body_fixed";
 
-const bodyNode = document.querySelector("body");
+const bodyNode2 = document.querySelector("body");
 const burgerNode = document.querySelector(".js-burger");
 const burgerBtnNode = document.querySelector(".js-burger-btn");
 const burgerContentNode = document.querySelector(".js-burger__content");
-const burgerLinkNode = document.querySelector(".js-link");
 
 burgerBtnNode.addEventListener("click", toggleBurger);
 
@@ -23,13 +21,6 @@ burgerNode.addEventListener("click", (event) => {
 
 function toggleBurger() {
   burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
-  bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+  bodyNode2.classList.toggle(BODY_FIXED_CLASSNAME_2);
   burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
-  burgerLinkNode.classList.toggle(BURGER_NAV_CLASSNAME);
 }
-
-document.querySelectorAll("[burger_link]").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburgerBtn.classList.remove("js-link");
-  })
-);
